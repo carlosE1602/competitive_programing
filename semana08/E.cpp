4d2ccd4ebc2bc4c7e3da3a7141e3d9b7af489579
carlosE1602 <carlosedu1602@gmail.com>
2022-12-12 08:43:59 -0300

    UPDATED - E.cpp Accepted

--- semana08/E.cpp
+++ semana08/E.cpp
@@ -9,40 +9,64 @@ void solve(int n){
     for(int i = 1;i<n;i++){
         cin >> _a >> _b;
         _a--; _b--;
-        cout << _a << " " << _b << "\n";
         m[_a].push_back(_b);
         m[_b].push_back(_a);
     }
-    cout << "fora\n";
 
     int t; cin >> t;
-    cout << t << "\n";
-    for(int i = 0;i<t;i++){
-        cout << "Entra\n";
-        t--;
+    while(t--){
         int a, b;
         cin >> a >> b;
         a--; b--;
-        cout << a << " " << b << "\n";
-        queue<int> fila;
-        cout << "proximo\n";
+        queue<pair<int,int>> fila;
 
         vector<int> vA(n,-1);
-        vector<int> vb(n,-1);
+        vector<int> vB(n,-1);
 
-        fila.push(a);
+        fila.push({a,1});
         vA[a] = 0;
-        int dist = 0;
         while(!fila.empty()){
-            frente = fila.front(); fila.pop();
-            dist++;
-            for(auto &i:m[frente]){
-                vA[i] = dist;
-                fila.push(i);
+            auto frente = fila.front(); fila.pop();
+            for(auto &i:m[frente.first]){
+                if(vA[i] == -1){
+                    vA[i] = frente.second;
+                    fila.push({i,frente.second+1});
+
+                }
+            }
+        }
+        fila.push({b,1});
+        vB[b] = 0;
+        int in = -1, out = -1;
+        bool ok = true;
+        while(!fila.empty()){
+            auto frente = fila.front(); fila.pop();
+            if(vA[frente.first] == vB[frente.first]){
+                cout << "The fleas meet at " << frente.first+1 << ".\n";
+                ok = false;break;
+            }
+            if((vA[frente.first]-vB[frente.first] )== 1){
+                cout << frente.first << "\n";
+                if(in == -1) in = frente.first+1;
+                else if(out == -1){out = frente.first+1;}
+            }
+            for(auto &i:m[frente.first]){
+                if(vB[i] == -1){
+                    vB[i] = frente.second;
+                    fila.push({i,frente.second+1});
+
+                }
             }
         }
 
+        if(ok){
+
+        cout << "The fleas jump forever between " << in << " " << out << ".\n";
+        }
         for(auto &i:vA) cout << i << " ";
+        cout << "\n";
+
+        for(auto &i:vB) cout << i << " ";
         cout << "\n";
     }
 
